9b2d820f6ebdafe32511ae77c7f235aa1046ffb4
asulon <asulon@student.42.fr>
2026-06-17 15:01:01 +0200

    implement pygame in project

--- fly_in.py
+++ fly_in.py
@@ -3,16 +3,17 @@
 #                                                      :::      ::::::::    #
 #  fly_in.py                                         :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: asulon <asulon@student.42nice.fr>         +#+  +:+       +#+         #
+#  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/21 16:39:37 by asulon          #+#    #+#               #
-#  Updated: 2026/05/25 22:39:17 by asulon          ###   ########.fr        #
+#  Updated: 2026/06/17 15:00:16 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 import sys
 import re
 from typing import Dict, Any
+import pygame
 
 
 class ConfigError(Exception):
@@ -135,9 +136,50 @@ def validate_config(config: Dict[str, Any]) -> Dict[str, Any]:
 
 def main():
     try:
-        raw_config = parse_config("./maps/easy/01_linear_path.txt")
-        config = validate_config(raw_config)
-        print(config)
+        # raw_config = parse_config("./maps/easy/01_linear_path.txt")
+        # config = validate_config(raw_config)
+        # print(config)
+        # pygame setup
+        pygame.init()
+        screen = pygame.display.set_mode((1280, 720))
+        clock = pygame.time.Clock()
+        running = True
+        dt = 0
+
+        player_pos = pygame.Vector2(
+            screen.get_width() / 2, screen.get_height() / 2)
+
+        while running:
+            # poll for events
+            # pygame.QUIT event means the user clicked X to close your window
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # fill the screen with a color to wipe away anything from last frame
+            screen.fill("purple")
+
+            pygame.draw.circle(screen, "red", player_pos, 40)
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                player_pos.y -= 300 * dt
+            if keys[pygame.K_s]:
+                player_pos.y += 300 * dt
+            if keys[pygame.K_a]:
+                player_pos.x -= 300 * dt
+            if keys[pygame.K_d]:
+                player_pos.x += 300 * dt
+
+            # flip() the display to put your work on screen
+            pygame.display.flip()
+
+            # limits FPS to 60
+            # dt is delta time in seconds since last frame, used for framerate-
+            # independent physics.
+            dt = clock.tick(60) / 1000
+
+        pygame.quit()
     except (ValueError, ConfigError):
         sys.exit(1)
 
